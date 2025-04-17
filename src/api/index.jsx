7566a6fa0464b74d22f24e83d3b69c66de323cd3
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2025-04-17 12:50:50 +0500

    problem fixed

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -8,4 +8,18 @@ const Api = axios.create({
   },
 });
 
+Api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(API_TOKEN);
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
 export default Api;
